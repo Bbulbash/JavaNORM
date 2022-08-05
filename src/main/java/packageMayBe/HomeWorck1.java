0b738e7648386d30a994fee934933f75e41fbb43
Bbulbash <mike.sm.01@yandex.ru>
2022-08-05 22:34:52 +0300

    Выполнил первый комит

---
+++ src/main/java/packageMayBe/HomeWorck1.java
@@ -0,0 +1,13 @@
+package packageMayBe;
+
+import javax.sound.midi.Soundbank;
+
+public class HomeWorck1 {
+    public static void main(String[] args) {
+        task1();
+    }
+
+    private static void task1() {
+        System.out.println("Решение");
+    }
+}
